5f166134ea07cc2e433072b5f07129aba685b0c7
Masoud-M <masoudmoghaddam999@gmail.com>
2023-09-30 14:59:13 +0330

    added some styling

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -6,15 +6,17 @@ function AppLayout() {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
 
       <Header />
 
-      <main>
-        {/* outlet component is provided by react-router-dom and its usage is to show the current child elements in the nested routes. works just like {children} in components*/}
-        <Outlet />
-      </main>
+      <div className="overflow-scroll ">
+        <main className="mx-auto max-w-3xl ">
+          {/* outlet component is provided by react-router-dom and its usage is to show the current child elements in the nested routes. works just like {children} in components*/}
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
