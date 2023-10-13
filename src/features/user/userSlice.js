0c0c5554e24367d146cc4c263b5c80bf3953efe0
Masoud-M <masoudmoghaddam999@gmail.com>
2023-10-13 13:34:51 +0330

    added geolocation option in the address input of submitting order to find user location easier

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,29 +1,38 @@
-// function getPosition() {
-//   return new Promise(function (resolve, reject) {
-//     navigator.geolocation.getCurrentPosition(resolve, reject);
-//   });
-// }
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getAddress } from "../../services/apiGeocoding";
+function getPosition() {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
 
-// async function fetchAddress() {
-//   // 1) We get the user's geolocation position
-//   const positionObj = await getPosition();
-//   const position = {
-//     latitude: positionObj.coords.latitude,
-//     longitude: positionObj.coords.longitude,
-//   };
+// since redux is synchronous in nature and we can not use async function within it, we use the built in createAsyncThunk function to make our fetchAddress action creator
+export const fetchAddress = createAsyncThunk(
+  "user/fetchAddress",
+  async function () {
+    // 1) We get the user's geolocation position
+    const positionObj = await getPosition();
+    const position = {
+      latitude: positionObj.coords.latitude,
+      longitude: positionObj.coords.longitude,
+    };
 
-//   // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
-//   const addressObj = await getAddress(position);
-//   const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+    // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+    const addressObj = await getAddress(position);
+    const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
 
-//   // 3) Then we return an object with the data that we are interested in
-//   return { position, address };
-// }
-
-import { createSlice } from "@reduxjs/toolkit";
+    // 3) Then we return an object with the data that we are interested in
+    // payload of the FULFILLED state
+    return { position, address };
+  },
+);
 
 const initialState = {
   username: "",
+  status: "idle",
+  position: {},
+  address: "",
+  error: "",
 };
 
 const userSlice = createSlice({
@@ -34,6 +43,23 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+
+  // thunk will produce 3 different action types that we need to handle these cases in our reducers. 1- pending promise state. 2- fulfilled state. 3- rejected state
+
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchAddress.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.position = action.payload.position;
+        state.address = action.payload.address;
+        state.status = "idle";
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        state.status = "error";
+        state.error = "There was a problem getting your address. Make sure to fill this field!";
+      }),
 });
 
 export const { updateName } = userSlice.actions;
